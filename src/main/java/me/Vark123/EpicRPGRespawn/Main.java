158ea9d79698049db70b0457b42630c6aaf0eae1
CopperV <vark123@interia.pl>
2023-06-29 16:47:28 +0200

    init commit

---
+++ src/main/java/me/Vark123/EpicRPGRespawn/Main.java
@@ -0,0 +1,22 @@
+package me.Vark123.EpicRPGRespawn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import lombok.Getter;
+
+@Getter
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
+
+}
